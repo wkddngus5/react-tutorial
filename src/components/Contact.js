dec5eca425026ec5686d9f29de6433e13a9b78d1
wkddngus5 <wkddngus5@naver.com>
2018-04-04 12:41:29 +0900

    feature/searching

---
+++ src/components/Contact.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import ContactInfo from './ContactInfo';
+
+export default class Contact extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedKey: -1,
+      keyword: '',
+      contactData: [{
+        name: 'Abet',
+        phone: '010-0000-0001'
+      }, {
+        name: 'Betty',
+        phone: '010-0000-0002'
+      }, {
+        name: 'Charlie',
+        phone: '010-0000-0003'
+      }, {
+        name: 'David',
+        phone: '010-0000-0004'
+      }]
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
+
+  handleClick(key) {
+    this.setState({
+      selectedKey: key
+    });
+    console.log(key, 'is selected');
+  }
+
+  render() {
+    const mapToComponents = (data) => {
+      data.sort();
+      data = data.filter(contact => {
+        return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+      });
+      return data.map((contact, i) => {
+        return (<ContactInfo
+          contact={contact}
+          key={i}
+          onclick={() => {console.log('A')}} />);
+      });
+    };
+
+    return (
+      <div>
+        <h1>Contacts</h1>
+        <input
+          name="keyword"
+          placeholder="Search"
+          value={this.state.keyword}
+          onChange={this.handleChange}
+        />
+        <div>{mapToComponents(this.state.contactData)}</div>
+      </div>
+    );
+  }
+}
